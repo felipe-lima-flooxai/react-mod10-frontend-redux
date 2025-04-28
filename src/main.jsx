42ab8e812c9b15b9120eca4d6b60cc21dc73c78b
felipe-lima-flooxai <felipe.lima.flooxai@gmail.com>
2025-04-28 09:43:16 -0300

    feat: store do redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {createStore} from "redux"
+import  {Provider} from "react-redux"
 
 import App from "./app.jsx"
+import reducers from "reducers.js"
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 // JavaScript (opcional, só se usar componentes JS)
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 
+const store = createStore(reducers)
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
-    <App></App>
+    <Provider store={store}>
+      <App></App>
+    </Provider>
 
   </React.StrictMode>
 )
